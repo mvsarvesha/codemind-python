5debc6f7df600d27fdddf7961486b8bd920b6f70
mvsarvesha <63770211+mvsarvesha@users.noreply.github.com>
2022-12-25 18:58:28 +0530

    nterms = int(input()) # first two terms n1, n2 = 0, 1 count = 0
    # check if the number of terms is valid
    if nterms == 1:
       print(n1)
    # generate fibonacci sequence
    else:
       while count < nterms:
           print(n1, end=" ")
           nth = n1 + n2
           # update values
           n1 = n2
           n2 = nth
           count += 1

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+nterms = int(input())
+# first two terms
+n1, n2 = 0, 1
+count = 0
+
+# check if the number of terms is valid
+if nterms == 1:
+   print(n1)
+# generate fibonacci sequence
+else:
+   while count < nterms:
+       print(n1, end=" ")
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
